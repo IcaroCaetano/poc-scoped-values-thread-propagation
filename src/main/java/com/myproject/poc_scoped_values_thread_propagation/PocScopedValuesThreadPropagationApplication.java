6f48008f157a7c34477137a1626a3e361c8ed4b4
ICAROCAETANO <icaro.caetano@gmail.com>
2026-05-19 22:29:33 -0300

    first commit

---
+++ src/main/java/com/myproject/poc_scoped_values_thread_propagation/PocScopedValuesThreadPropagationApplication.java
@@ -0,0 +1,13 @@
+package com.myproject.poc_scoped_values_thread_propagation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocScopedValuesThreadPropagationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocScopedValuesThreadPropagationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myproject/poc_scoped_values_thread_propagation/PocScopedValuesThreadPropagationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.poc_scoped_values_thread_propagation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocScopedValuesThreadPropagationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
